6b030f4b3bc37614176fb60cb216a49c95359025
josemarisarria <jmsarria74@gmai.com>
2023-11-24 13:17:34 +0100

    Corregido error indentación

--- apy.py
+++ apy.py
@@ -3,6 +3,6 @@ from src import covid_dash, hospitals_tbgit
 app = Flask(__name__)
 @app.route("/")
 def landing_page():
-return render_template('index.html')
-if __name__ == '__main__':
-app.run(host='0.0.0.0',port=1991, debug=True)
+    return render_template('index.html')
+    if __name__ == '__main__':
+        app.run(host='0.0.0.0',port=1991, debug=True)
